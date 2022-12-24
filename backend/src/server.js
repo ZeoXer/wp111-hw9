2890f17bbf22d11db24290440f1fc9d203091f38
ZeoXer <andrew17413@gmail.com>
2022-12-24 14:46:20 +0800

    update Dockerfile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,28 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import path from "path";
 import db from "./db";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 const port = process.env.PORT || 4000;
+
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+}
+app.use(express.json());
 
 db.connect();
 
 app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_URL =
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:4000/";
+
 const instance = axios.create({
-  baseURL: `http://localhost:4000/`,
+  baseURL: API_URL,
 });
 
 export default instance;

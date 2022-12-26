8ccd252c28f7c222d024f7c72311ce98040881d2
ZeoXer <89142542+ZeoXer@users.noreply.github.com>
2022-12-26 12:58:00 +0800

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 export default {
   connect: () => {
     mongoose
-      .connect("mongodb+srv://ZeoXer:hw6practice@cluster0.xlzpviw.mongodb.net/?retryWrites=true&w=majority", {
+      .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
